4cca4718b2acabb3ba97580d4156e74812a3bbf8
Greenwalt <KG155781@davenportschools.org>
2019-05-20 13:13:46 -0500

    update

--- src/SlapJack.java
+++ src/SlapJack.java
@@ -13,11 +13,9 @@ public class SlapJack {
         displayfourloop();
         Scanner input = new Scanner(System.in);
 
-        System.out.println("This is a game called slap jack. It is a fun card game");
-        System.out.println("The objective of the game is to get the most jacks out of the all the players.");
-        System.out.println("Hope you have fun and enjoy the game");
-        System.out.println(" Jack = 1 To slap press 2");
+        Rules.displayRulesFirstPart();
         int random = (int) (Math.random() * 3 + 1);
+
 
 
 
