03580d8fd0f9ac5a5d8653e70bc7209f285f950d
jverea <jvereagovan2018@gmail.com>
2019-05-20 13:13:51 -0500

    update

--- src/SlapJack.java
+++ src/SlapJack.java
@@ -1,31 +1,13 @@
 
-public class SlapJack {
+//public class SlapJack {
 
-    public static void main(String[] args) {
-        int cards;
-        int jack;
-        int deal;
-        int slap;
-        displayfourloop();
-
-        System.out.println("This is a game called slap jack. ");
-    }
-
-    public static void displayfourloop() {
-        while (user != 999)
-        {
-            if (user == 1) {
-                if (random == 1)
-                    result = "tie";
-                else if (random == 2)
-                    result = "Lose";
-                else
-                    result = "Win";
-            } else if (user == 2) {
-                if (random == 1) {
-                    result = "Win";
-                }
-            }
-        }
-    }
-}
+//    public static void main(String[] args) {
+//        int cards;
+//        int jack;
+//        int deal;
+//        int slap;
+//        int displayfourloop;
+//        displayfourloop();
+//        System.out.println("This is a game called slap jack. ");
+//    }
+// }
