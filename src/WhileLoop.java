1e0895c176fa8d07dc3bea6a721797ae51afb1d9
jverea <jvereagovan2018@gmail.com>
2019-05-21 13:00:34 -0500

    UPDATE

---
+++ src/WhileLoop.java
@@ -0,0 +1,16 @@
+//public class WhileLoop {
+//    public static void main(String[] args) {
+//
+//        while (count != 0) {
+//
+//            System.out.println(count);
+//            --count;
+//        }
+//        int Count2 = 0;
+//        while (Count2 <= 10) {
+//            System.out.println(Count2);
+//            ++Count2;
+//        }
+//    }
+//}
+
