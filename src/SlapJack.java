7f9b1a5029d81088e9626aea96ae1abd39cb0f02
jverea <jvereagovan2018@gmail.com>
2019-05-21 12:05:56 -0500

    final

--- src/SlapJack.java
+++ src/SlapJack.java
@@ -1,17 +1,4 @@
 
-//public class SlapJack {
-
-//    public static void main(String[] args) {
-//        int cards;
-//        int jack;
-//        int deal;
-//        int slap;
-//        int displayfourloop;
-//        displayfourloop();
-//        System.out.println("This is a game called slap jack. ");
-//    }
-// }
-
 import java.util.Scanner;
 
 
