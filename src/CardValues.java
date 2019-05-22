dd83dbd59a6bf49f725ad92ce0e9873dd47b8de8
Greenwalt <KG155781@davenportschools.org>
2019-05-22 12:05:43 -0500

    Create CardValues.java

---
+++ src/CardValues.java
@@ -0,0 +1,21 @@
+public class CardValues {
+
+
+    public static void displayArray() {
+        double[] cards = {52};
+        int total = 0;
+
+        displayFor(cards);
+
+    }
+
+    public static void displayFor(double[] cards) {
+        for (int i = 0; i < cards.length; i++) {
+            System.out.println(cards[i]);
+        }
+        for (int j = cards.length - 1; j >= 0; j--) {
+            System.out.println(cards[j]);
+        }
+    }
+
+}
