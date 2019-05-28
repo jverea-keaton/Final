26bcd373db5c3db8755a6d10fc615095ab80b557
jverea <jvereagovan2018@gmail.com>
2019-05-28 12:44:20 -0500

    update

--- src/WhileLoop.java
+++ src/WhileLoop.java
@@ -2,16 +2,15 @@ public class WhileLoop {
     public static void main(String[] args) {
 int count = 0;
 
-        while (count != 0) {
+        while (count != 1) {
 
             System.out.println(count);
             --count;
         }
-        int Count2 = 0;
+        int Count2 = 2;
         while (Count2 <= 52) {
             System.out.println(Count2);
             ++Count2;
         }
     }
 }
-
