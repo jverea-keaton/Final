4e35ca9ea4407421c74f1ef3bbafeab942cce12a
jverea <jvereagovan2018@gmail.com>
2019-05-30 11:59:40 -0500

    update

--- src/Sticks.java
+++ src/Sticks.java
@@ -1,32 +1,34 @@
 import java.util.Scanner;
 
 public class Sticks {
-    public static void displaysticks(){
+    public static void displaysticks() {
         int quit = 0;
         int grabSticks = 0;
+        int count = 0;
 
         int sticks = 21;
         Scanner input = new Scanner(System.in);
 
-        while(quit != 999) {
+        while (quit != 999) {
 
             System.out.println("There are " + sticks + " sticks");
             System.out.println("Choose to take 1 or 2 sticks out of the pile or 999 to quit");
             grabSticks = input.nextInt();
             if (grabSticks >= 2) {
-                sticks = sticks -2;
+                sticks = sticks - 2;
                 System.out.println(sticks);
             } else if (grabSticks <= 1) {
-                sticks = sticks -1;
+                sticks = sticks - 1;
                 System.out.println(sticks);
             }
-            if(sticks < 1){
+            if (sticks < 1) {
                 quit = 999;
                 System.out.println("You Suck!!!!!");
+                while (count != 0) {
+                    count = count + 1;
+                    System.out.println(count);
+                }
             }
         }
-
-
-
     }
 }
